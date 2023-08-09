f5927ea2a3eec6f2d48e6334673c1c543ba5728d
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-08-09 20:44:02 +0530

    Update wait.js

--- wait.js
+++ wait.js
@@ -1,6 +1,6 @@
 let wait = function (milliseconds) {
   return new Promise((resolve) => {
-    if (typeof milliseconds !== 'number') {
+    if (typeof milliseconds !== "number") {
       throw new Error('milliseconds not a number');
     }
     setTimeout(() => resolve("done!"), milliseconds)
